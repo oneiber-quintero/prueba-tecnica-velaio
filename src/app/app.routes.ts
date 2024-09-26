5c5c43c853c23fdcbcfcecf7688d82d3207f5f79
Oneiber Quintero <oneiber.37.37@gmail.com>
2024-09-26 17:51:43 -0400

    add router and create tasks component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/tasks/tasks.component').then(m => m.TasksComponent)
+  }
+];
